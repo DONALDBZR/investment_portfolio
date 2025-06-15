c0d7ca4824e085f3167fb2971808db28e9e031bb
DONALDBZR <andygaspard@hotmail.com>
2025-06-15 13:50:56 +0400

    [UPDATE 0.0.12] Component providing configuration for FinClub-related external uniform resource locators.

---
+++ src/main/java/com/investment_portfolio/controller/FinClubController.java
@@ -0,0 +1,14 @@
+package com.investment_portfolio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Component providing configuration for FinClub-related external uniform resource locators.
+ * @author Andy Ewen Gaspard (Darkness4869)
+ */
+@RestController
+@RequestMapping("/FinClub")
+public class FinClubController {}
