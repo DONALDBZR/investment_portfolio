acb8b5307d4c6abc0c7e526a7d557b3d3e7fab01
DONALDBZR <andygaspard@hotmail.com>
2025-06-15 13:53:58 +0400

    [UPDATE 0.0.13] The constructor which sets the base uniform resource locator.

--- src/main/java/com/investment_portfolio/controller/FinClubController.java
+++ src/main/java/com/investment_portfolio/controller/FinClubController.java
@@ -11,4 +11,24 @@ import org.springframework.stereotype.Component;
  */
 @RestController
 @RequestMapping("/FinClub")
-public class FinClubController {}
+public class FinClubController {
+    /**
+     * The base uniform resource locator that is used by the application to communicate with the service needed.
+     */
+    private String base_uniform_resource_locator;
+
+    /**
+     * The constructor which sets the base uniform resource locator.
+     */
+    public FinClubController() {
+        this.setBaseUniformResourceLocator("https://finclub.mu:8080");
+    }
+
+    private String getBaseUniformResourceLocator() {
+        return this.base_uniform_resource_locator;
+    }
+
+    private void setBaseUniformResourceLocator(String base_uniform_resource_locator) {
+        this.base_uniform_resource_locator = base_uniform_resource_locator;
+    }
+}
