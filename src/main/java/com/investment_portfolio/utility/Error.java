d6404a7de434a089893ea634b2f3b11e6c2c2fdc
DONALDBZR <andygaspard@hotmail.com>
2025-06-29 12:52:59 +0400

    [UPDATE 0.1.35] Utility class for handling error-related logging and operations.

---
+++ src/main/java/com/investment_portfolio/utility/Error.java
@@ -0,0 +1,20 @@
+package com.investment_portfolio.utility;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Utility class for handling error-related logging and operations.
+ * <p>This class provides a centralized logger for tracking error events in the application.</p>
+ * <p>The class is marked as final to prevent inheritance.</p>
+ */
+public final class Error {
+    /**
+     * The logger that is responsible of tracking the actions on the application.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(Error.class);
+}
